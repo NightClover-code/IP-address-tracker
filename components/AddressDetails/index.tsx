4fa761a3ad3beacbc25f44019753f64485b6a720
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-08-25 15:15:19 +0100

    adding item

--- components/AddressDetails/index.tsx
+++ components/AddressDetails/index.tsx
@@ -4,8 +4,11 @@ const AddressDetails = () => {
   return (
     <div className="address__details">
       <Item headline="IP Address" data="192.212.174.101" />
+      <div className="vertical__line"></div>
       <Item headline="Location" data="Brooklyn, NY, 10001" />
+      <div className="vertical__line"></div>
       <Item headline="TimeZone" data="UTC-05:00" />
+      <div className="vertical__line"></div>
       <Item headline="ISP" data="SpaceX Starlink" />
     </div>
   );
