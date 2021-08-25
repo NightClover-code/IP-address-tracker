937b9cd880e81745be2bf6c781c3c2c003de2e01
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-08-25 01:39:04 +0100

    adding a map component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,17 @@ import { NextPage } from 'next';
 import Header from '../components/Header';
 import SEO from '../components/SEO';
 import { seoConfig } from '../utils';
+import dynamic from 'next/dynamic';
 
 const HomePage: NextPage = () => {
+  const Map = dynamic(() => import('../components/Map'), { ssr: false });
+
   return (
     <>
       <SEO {...seoConfig} />
       <div className="app__container">
         <Header />
+        <Map />
       </div>
     </>
   );
